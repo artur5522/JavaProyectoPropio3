6d45401e13c86913e78b3ef1cf82d5b3cd5c1f9d
ARTURO FERNANDO <73620010+artur5522@users.noreply.github.com>
2021-10-16 12:22:27 -0300

    Add files via upload

---
+++ src/main/java/com/rodrigueza/Entities/NewMain.java
@@ -0,0 +1,84 @@
+package com.rodrigueza.Entities;
+
+import com.rodrigueza.orm.Gestor;
+import com.rodrigueza.Validations.Validations;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Elpo
+ */
+public class NewMain {
+
+    public static void main(String[] args) {
+
+        Gestor gestor = new Gestor();
+        gestor.creaSession();        
+        
+        Computadora compu = creaCompu();
+        insertaDatoComputadora(gestor, compu);
+             
+        
+        gestor.closeSession();       
+        gestor=null;  
+        
+    }
+
+    public static Computadora creaCompu() {
+        Scanner sc = new Scanner(System.in);
+        Computadora compu = new Computadora();      
+        String decision = null;
+        System.out.println("Ingrese los datos de la computadora a persistir:");
+        System.out.println("Ingrese el nombre de la marca:");
+        decision = Validations.validaNombre();
+        compu.setMarca(decision);
+        System.out.println("Ingrese el nombre del modelo:");
+        decision = Validations.validaNombre();
+        compu.setModelo(decision);   
+        System.out.println("Introduzca el codigo: ");
+        decision = String.valueOf(Validations.validaNumero());      
+        compu.setCodigo(decision);       
+        System.out.println("Ingrese el/los componentes asociados a esta computadora:");
+        while (true) {
+            Componente compo = null;
+            int cuentaComponentes = 1;
+            System.out.println("Ingrese el componente -> " + cuentaComponentes);
+            cuentaComponentes++;
+            compo = creaComponentes();
+            compu.agregaComponentes(compo);
+            System.out.println("Desea agregar otro componente? S/N");
+            String continuar = sc.nextLine();
+            if (continuar.equalsIgnoreCase("n")) {
+                System.out.println("Hemos terminado de brindar informacion. \n");
+                break;
+            } else {
+                System.out.println("Continuemos con el siguiente componente..");
+            }
+        }
+        return compu;
+    }
+
+    public static Componente creaComponentes() {
+        Componente compo = new Componente();
+        String decision = "";
+        Scanner sc = new Scanner(System.in);       
+        System.out.println("Ingrese nombre del componente:");
+        decision = Validations.validaNombre();
+        compo.setNombre(decision);
+        System.out.println("Ingrese numero de serie:");
+        decision = String.valueOf(Validations.validaNumero()); 
+        compo.setNroSerie(decision);
+        return compo;
+    }    
+    
+    public static void insertaDatoComputadora(Gestor gestor, Computadora compu){
+        try {
+             gestor.guardar(compu);            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+
+}
---
+++ src/main/java/com/rodrigueza/Validations/Validations.java
@@ -0,0 +1,50 @@
+/*
+ * ARCHIVO DONDE SE ENCUENTRAN TODAS LAS VALIDACIONES NECESARIAS PARA REALIZAR LAS
+ENTRADAS DE DATOS 
+ */
+package com.rodrigueza.Validations;
+
+import java.util.*;
+
+/**
+ *
+ * @author User
+ */
+public class Validations {
+
+    public static String validaNombre() {
+        Scanner sc = new Scanner(System.in);
+        String nombre = "";
+        //valido que primer nombre y primer apellido no esten vacios
+        while (true) {
+            nombre = sc.nextLine();
+            if (nombre.length() != 0) {
+                break;
+            } else {
+                System.out.println("El nombre no puede estar vacio. Coloquelo nuevamente");
+            }
+        }
+        return nombre.toUpperCase();
+    }
+
+    public static long validaNumero() {
+        Scanner sc = new Scanner(System.in);
+        long codigo = 0;
+        //valido que se ingrese un numero, y que ese numero sea de 8 caracteres
+        while (true) {
+            try {
+                codigo = sc.nextLong();
+                String codigoString = String.valueOf(codigo);
+                if (codigoString.length() < 1 || codigo <= 0) {
+                    System.out.println("El codigo no puede estar vacío ni ser menor que cero. Intente nuevamente");
+                } else {
+                    return codigo;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error. Ingrese un numero valido");
+                sc.next();
+            }
+        }
+
+    }
+}
