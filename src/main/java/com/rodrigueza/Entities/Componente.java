cda7dd6f44328e61808443561c06b2e8c8f96db1
ARTURO FERNANDO <73620010+artur5522@users.noreply.github.com>
2021-10-16 12:19:51 -0300

    Add files via upload

---
+++ src/main/java/com/rodrigueza/Entities/Componente.java
@@ -0,0 +1,74 @@
+package com.rodrigueza.Entities;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Elpo
+ */
+@Entity
+@Table(name = "componente")
+public class Componente implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private BigInteger id;
+
+    @Column(name = "nro_serie")
+    private String nroSerie;
+    
+    private String nombre;
+  
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.MERGE})
+    @JoinColumn(name = "fk_id_computadora")
+    private Computadora idComputadora;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNroSerie() {
+        return nroSerie;
+    }
+
+    public void setNroSerie(String nroSerie) {
+        this.nroSerie = nroSerie;
+    }
+
+
+    public Computadora getIdComputadora() {
+        return idComputadora;
+    }
+
+    public void setIdComputadora(Computadora idComputadora) {
+        this.idComputadora = idComputadora;
+    }
+
+    @Override
+    public String toString() {
+        return "Componente{" + "id=" + id + ", nroSerie=" + nroSerie + ", nombre=" + nombre + ", idComputadora=" + idComputadora + '}';
+    }
+    
+    
+    
+}
